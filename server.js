62e3dd39cbf54796c189b33cdf9e272810d3eda3
Sankalp <sankalppatel38@gmail.com>
2023-01-10 12:38:29 +0530

    Backend Dog File

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 // server.js
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+const router = jsonServer.router(path.join(__dirname, "db.json"));
 const MiddleWares = jsonServer.defaults()
 
 server.use(MiddleWares)
-server.use(router)
+server.use(jsonServer.bodyParser);
+server.use("/api/v1", router);
 server.listen(() => 
 {
   console.log('JSON Server is running')
